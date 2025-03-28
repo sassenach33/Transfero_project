3cae2086d22e84f9614a985ead9a43bd186eac06
Dr William <drwilliam@havard.com>
2025-03-28 16:51:13 -0300

    Alterações finais do dia 28/03/2025

--- sistema/models.py
+++ sistema/models.py
@@ -13,5 +13,28 @@ class Usuario(models.Model):
     endereco = models.CharField(max_length=100)
     data_cadastro = models.DateTimeField(default=timezone.now)
     ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome}'
     
+class Filme(models.Model):
+    nome = models.CharField(max_length=50)
+    ano = models.DateField(default=timezone.now)
+    estudio = models.CharField(max_length=50)
+    genero = models.CharField(max_length=50)
+    sinopse = models.TextField()
+    data_cadastro = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.nome
     
+class Genero(models.Model):
+    nome = models.CharField(max_length=50)
+    data_cadastro = models.DateField(default=timezone.now)
+    
+    def __str__(self):
+        return self.nome
+
+
+# Filme - Nome do Filme, ano de lançamento, estúdio, gênero, sinopse, data de cadastro
+# Gênero - nome, data de cadastro.
