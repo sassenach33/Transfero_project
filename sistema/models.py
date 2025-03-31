9f22c145a8314dc392581890d018837ec7259c84
Dr William <drwilliam@havard.com>
2025-03-31 16:44:38 -0300

    Alterações do dia 31/03/2025

--- sistema/models.py
+++ sistema/models.py
@@ -13,20 +13,10 @@ class Usuario(models.Model):
     endereco = models.CharField(max_length=100)
     data_cadastro = models.DateTimeField(default=timezone.now)
     ativo = models.BooleanField(default=True)
+    imagem = models.ImageField(blank=True, upload_to='imagens/%Y/%m')
 
     def __str__(self):
         return f'{self.nome} {self.sobrenome}'
-    
-class Filme(models.Model):
-    nome = models.CharField(max_length=50)
-    ano = models.DateField(default=timezone.now)
-    estudio = models.CharField(max_length=50)
-    genero = models.CharField(max_length=50)
-    sinopse = models.TextField()
-    data_cadastro = models.DateField(default=timezone.now)
-
-    def __str__(self):
-        return self.nome
     
 class Genero(models.Model):
     nome = models.CharField(max_length=50)
@@ -35,6 +25,23 @@ class Genero(models.Model):
     def __str__(self):
         return self.nome
 
+class Filme(models.Model):
+    nome = models.CharField(max_length=50)
+    ano = models.DateField(default=timezone.now)
+    estudio = models.CharField(max_length=50)
+    # genero = models.CharField(max_length=50)
+    genero = models.ForeignKey(Genero, on_delete=models.SET_NULL, null=True, blank=True)
+    sinopse = models.TextField()
+    data_cadastro = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.nome
+    
+
+
 
 # Filme - Nome do Filme, ano de lançamento, estúdio, gênero, sinopse, data de cadastro
 # Gênero - nome, data de cadastro.
+
+# PK -> Primary key -> Chave primária.
+# FK -> Foreign Key -> Chave estrangeira.
