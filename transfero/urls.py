4464c203b1e951fcaa35a9f029289ef0fd193e63
Dr William <drwilliam@havard.com>
2025-05-06 14:10:29 -0300

    Alterações do dia 06/05/2025

--- transfero/urls.py
+++ transfero/urls.py
@@ -4,8 +4,12 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sistema.urls')),
+    path('usuario/', include('usuarios.urls')),
 ]
 
 # 127.0.0.1:8000/ => A página principal
 # 127.0.0.1:8000/admin => A página da tela do django admin
+
+
+
 
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from usuarios import views
 
 urlpatterns = [
-    path('cadastro/', cadastro, name='cadastro'),
-    path('login/', login, name="login"),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('login/', views.login, name='login'),
 ]
+
+# 127.0.0.1:8000/usuario
+# 127.0.0.1:8000/usuario/login
+# 127.0.0.1:8000/usuario/cadastro
