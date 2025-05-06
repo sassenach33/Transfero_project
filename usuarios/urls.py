7db04ff8a3af50ebaa5fa7d221771ca1f888391c
Dr William <drwilliam@havard.com>
2025-05-06 16:50:48 -0300

    Alterações finais da aula do dia 06/05/2025

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from usuarios import views
 
 urlpatterns = [
-    path('cadastro/', views.cadastro, name='cadastro'),
+    path('cadastro/', views.criarUsuario, name='criarUsuario'),
     path('login/', views.login, name='login'),
 ]
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from usuarios.forms import UsuarioForm
 
 # Create your views here.
-
-def cadastro(request):
-    return render(
-        request,
-        'cadastro.html'
-    )
 
 def login(request):
     return render(
         request,
         'login.html'
+    )
+
+def criarUsuario(request):
+    # Verificar se a requisição será do tipo GET ou POST
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST, request.FILES)    
+        # Será criado um objto Usuario(model) com dos dados enviados.
+        # post -> São os campos do forms (nome, sobrenome) preenchidos.
+        # files -> Contém os arquivos ou e/imagens. 
+        if form.is_valid(): # Se os dados forem válidos, são salvos no BD.
+            form.save()
+            return redirect('/usuario/login')
+    
+    else:
+        # Se a requisição for GET, exibir o formulário de cadastro
+        form = UsuarioForm()
+    
+    return render(
+        request,
+        'cadastro.html',
+        {'form': form}
     )
